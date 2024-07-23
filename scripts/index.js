07217b1f345a7c8168039dc42e358e54d07f20d8
Raúl Fabra <fabraraul@yahoo.com>
2024-07-23 14:29:52 +0200

    Icon Menu mobile

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,16 @@
-const bottonMenú = document.querySelector('.header__menuBar')
-const menu = document.querySelector('.nav')
+const button = document.querySelector('.menuBar--button')
+const imagen = button.childNodes[1]
+const navBar = document.querySelector('.nav')
 
-bottonMenú.addEventListener('click', () => {
-    console.log(bottonMenú.ATTRIBUTE_NODE)
-    menu.classList.toggle('nav--mobile')
+const crossIcon = './public/images/cross.png'
+const burgerIcon = './public/images/hamburger.png'
+
+button.addEventListener('click', () => {
+    navBar.classList.toggle('nav--mobile')
+    if(navBar.classList.contains('nav--mobile')) {
+        imagen.src = crossIcon
+        imagen.width = '20'
+        imagen.height = '13'
+    }
+    else imagen.src = burgerIcon
 })
