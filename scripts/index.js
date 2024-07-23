4a36e81b5f665a24705efc8e78a4bd8e82c1b33f
Raúl Fabra <fabraraul@yahoo.com>
2024-07-23 13:50:02 +0200

    Add changes in Header, like Html, Css

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
-const bottonMenú = document.querySelector('.menu-bar')
+const bottonMenú = document.querySelector('.header__menuBar')
 const menu = document.querySelector('.nav')
 
 bottonMenú.addEventListener('click', () => {
+    console.log(bottonMenú.ATTRIBUTE_NODE)
     menu.classList.toggle('nav--mobile')
 })
