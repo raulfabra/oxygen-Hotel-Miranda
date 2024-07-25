b66569bf2d62adcf059116c5610864a71253a5f4
Raúl Fabra <fabraraul@yahoo.com>
2024-07-25 15:14:38 +0200

    Already finish About Us Page. 85% yet

--- scripts/swiper-about.js
+++ scripts/swiper-about.js
@@ -1,3 +1,23 @@
+const rateSwiper = new Swiper('.facilities__swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.facilities__swiper__pagination',
+    bulletClass: 'facilities__swiper__bullet',
+    bulletActiveClass: 'facilities__swiper__bullet--active',
+    clickable: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.facilities__swiper__btn--next',
+    prevEl: '.facilities__swiper__btn--prev',
+  },
+  
+});
 /* const rateSwiper = new Swiper('.facilities__swiper', {
   // Optional parameters
   direction: 'horizontal',
