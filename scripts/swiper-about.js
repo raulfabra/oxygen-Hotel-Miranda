3d86e47b3f8f8447e730b337db9e1bd1e76dc76a
Raúl Fabra <fabraraul@yahoo.com>
2024-07-25 08:52:47 +0200

    Fixed Swiper estructure and styles

---
+++ scripts/swiper-about.js
@@ -0,0 +1,28 @@
+/* const rateSwiper = new Swiper('.facilities__swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 30,
+
+  pagination: {
+      el: '.facilities__pagination',
+      bulletClass: 'facilities__darkbullet',
+      bulletActiveClass: 'facilities__bullet--active',
+      clickable: true,
+    },
+});
+
+
+const imagesSwiper = new Swiper('.about__images__swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 30,
+
+  pagination: {
+      el: '.about__images__pagination',
+      bulletClass: 'about__images__bullet',
+      bulletActiveClass: 'about__images__bullet--active',
+      clickable: true,
+    },
+}); */
