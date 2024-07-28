c2452e444cbf3110e46d505d997a25c5f03dc68d
Raúl Fabra <fabraraul@yahoo.com>
2024-07-28 12:16:52 +0200

    remove Pictures to keep on optimizate webApp and Fixed slider in Index.html

--- scripts/swiper-index.js
+++ scripts/swiper-index.js
@@ -15,15 +15,15 @@ const swiper = new Swiper('.carrusel', {
     },
 })
 
-const roomSwiper = new Swiper('.rooms__swiper', {
+const roomSwiper = new Swiper('.rooms__swiper--white', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
 
     // Navigation arrows
     navigation: {
-      nextEl: '.rooms__swiper__btn--next',
-      prevEl: '.rooms__swiper__btn--prev',
+      nextEl: '.rooms__swiper__white__btn--next',
+      prevEl: '.rooms__swiper__white__btn--prev',
     }
 })
 
