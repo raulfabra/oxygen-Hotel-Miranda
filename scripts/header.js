0e16f38b2624293485e4aa87e028f04fd30e9438
Raúl Fabra <fabraraul@yahoo.com>
2024-09-02 18:40:25 +0200

    Update Swiper slider to grid in Facilities section

--- scripts/header.js
+++ scripts/header.js
@@ -1,29 +1,38 @@
-const button = document.querySelector('.menuBar--button');
+const button = document.querySelector(".menuBar--button");
 const imagen = button.childNodes[1];
-const navBar = document.querySelector('.nav');
+const navBar = document.querySelector(".nav");
 
-const crossIcon = '../public/images/cross.png';
-const burgerIcon = '../public/images/hamburger.png';
+const crossIcon = "../public/images/cross.png";
+const burgerIcon = "../public/images/hamburger.png";
 
-button.addEventListener('click', () => {
-  navBar.classList.toggle('nav--mobile');
+const headerBox = document.querySelector(".header");
+const headerMenu = document.querySelector(".header__menuBar");
 
-  if (navBar.classList.contains('nav--mobile')) {
+/* const mediaQuery = window.matchMedia("(min-width: 1000px)"); */
+
+button.addEventListener("click", () => {
+  navBar.classList.toggle("nav--mobile");
+
+  if (navBar.classList.contains("nav--mobile")) {
     imagen.src = crossIcon;
-    imagen.width = '20';
-    imagen.height = '13';
+    imagen.width = "20";
+    imagen.height = "13";
   } else imagen.src = burgerIcon;
 });
 
-const headerBox = document.querySelector('.header');
-const headerMenu = document.querySelector('.header__menuBar');
-
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   if (window.scrollY > 5) {
-    headerBox.classList.add('header__hidden');
-  } else headerBox.classList.remove('header__hidden');
+    headerBox.classList.add("header__hidden");
+  } else headerBox.classList.remove("header__hidden");
 });
 
-headerBox.addEventListener('mousemove', function (event) {
-  headerBox.classList.remove('header__hidden');
+headerBox.addEventListener("mousemove", function (event) {
+  headerBox.classList.remove("header__hidden");
+});
+
+window.addEventListener("resize", () => {
+  if (mediaQuery.matches === true) {
+    navBar.classList.remove("nav--mobile");
+    imagen.src = burgerIcon;
+  }
 });
